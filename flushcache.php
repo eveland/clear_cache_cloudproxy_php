b9287bc4ef46461e43feba7cd66672794633c881
Salvador Aguilar S <sal.aguilar81@gmail.com>
2016-10-18 18:40:02 -0600

    First commit

---
+++ flushcache.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Simple Script in PHP to 
+ * 
+ * Author: Salvador Aguilar
+ * Email: sal.aguilar81@gmail.com
+ * Web: salrocks.com
+ */
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://waf.sucuri.net/api?v2',
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => array(
+        // this is the Sucuri CloudProxy API key for this website
+        k => 'your-cloudproxy-api-key-goes-here',
+        // this is the Sucuri CloudProxy API secret for this website
+        s => 'your-cloudproxy-api-secret-goes-here',
+        // this is the Sucuri CloudProxy API action for this website
+        a => 'clear_cache'
+    )
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+
+echo '<pre>' . $resp . '</re>';
+// Close request to clear up some resources
+curl_close($curl);
---
+++ readme.md
@@ -0,0 +1,14 @@
+Simple Cloudproxy Clear Cache via PHP
+=====================================
+
+This is a simple script in PHP to clear your Sucuri's CloudProxy cache. Avoid having to login to Sucuri's dashboard and simply run this script to clear the cache for your website. Simple & easy!
+
+Just login to your Sucuri's CloudProxy Dashboard. Then select the website you want to setup and go to the API and click on the API tab on that page. You only need to setup the API key and API secret and you are ready to roll!
+
+If you face issues, just let me know!
+
+##Author:
+[Salvador Aguilar](https://github.com/riper81)
+*twitter: [@riper81](https://twitter.com/RipeR81)
+*web: [salrocks.com](https://www.salrocks.com)
+*email: [sal.aguilar81@gmail.com](mailto:sal.aguilar81@gmail.com)
